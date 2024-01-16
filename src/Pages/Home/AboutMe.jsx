78c39ec437635b23983f3b81f5a505579f8d975f
nguyenkayla <69479689+nguyenkayla@users.noreply.github.com>
2024-01-16 16:17:09 -0500

    Update AboutMe.jsx

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
     return (
         <section id="AboutMe" className="about--section">
             <div className="about--section--img">
-                <img src="public/img/about.png" alt="About Me"></img>
+                <img src="./img/about.png" alt="About Me"></img>
             </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
