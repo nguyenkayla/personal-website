034daa52b034b1d1a07183e6a07a0dcd6b01b83b
Kayla Nguyen <69479689+nguyenkayla@users.noreply.github.com>
2025-06-09 14:25:35 -0400

    Update HeroSection.jsx

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
                 </a>
             </div>
             <div className="hero--section--img">
-                <img src="./img/mainImage.jpeg" alt="Hero Selection"></img>
+                <img src="./img/pic.png" alt="Hero Selection"></img>
             </div>
         </section>
     );
