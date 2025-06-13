f505dc470fedc1c11a8bf3909beb8f2d365828c4
Kayla Nguyen <69479689+nguyenkayla@users.noreply.github.com>
2025-06-13 12:47:46 -0400

    Update HeroSection.jsx

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,9 +5,9 @@ export default function HeroSection() {
                 <div className="hero--section--content">
                 <p className="section--title">Hello, I'm Kayla!</p>
                 <h2 className="hero--section--title">
-                <span className="hero--section-title--color">Software</span>{" "}
+                <span className="hero--section-title--color">Tech</span>{" "}
                 <br />
-                Developer
+                Enthusiast
                 </h2>
                         <p className="hero--section-description">
                             I am a senior at the University of Massachusetts Amherst majoring in Computer Science and graduating in 2026. I am passionate about building real-world applications that draw on my knowledge of data science and research.
